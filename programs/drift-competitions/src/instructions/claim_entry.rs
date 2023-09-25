39c3ecfcb0d1cae7c767d011f26763dc8c2eb1b7
Chris Heaney <chrisheaney30@gmail.com>
2023-09-25 16:03:30 -0400

    only 1 claim entry per ix

--- programs/drift-competitions/src/instructions/claim_entry.rs
+++ programs/drift-competitions/src/instructions/claim_entry.rs
@@ -1,10 +1,25 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::sysvar::instructions;
 
 use super::constraints::*;
 use crate::state::{Competition, Competitor};
 use drift::state::user::UserStats;
+use crate::error::ErrorCode;
 
-pub fn claim_entry<'info>(_ctx: Context<'_, '_, '_, 'info, ClaimEntry<'info>>) -> Result<()> {
+pub fn claim_entry<'info>(ctx: Context<'_, '_, '_, 'info, ClaimEntry<'info>>) -> Result<()> {
+    let ixs = ctx.accounts.instructions.as_ref();
+    let current_index = instructions::load_current_index_checked(ixs)? as usize;
+
+    if current_index != 0 {
+        msg!("claim entry must be first ix");
+        return Err(ErrorCode::Default.into());
+    }
+
+    if instructions::load_instruction_at_checked(1, ixs).is_ok() {
+        msg!("claim entry must be only ix");
+        return Err(ErrorCode::Default.into());
+    }
+
     Ok(())
 }
 
@@ -26,4 +41,7 @@ pub struct ClaimEntry<'info> {
         constraint = is_user_stats_for_competitor(&competitor, &drift_user_stats)?
     )]
     pub drift_user_stats: AccountLoader<'info, UserStats>,
+    /// CHECK: fixed instructions sysvar account
+    #[account(address = instructions::ID)]
+    pub instructions: UncheckedAccount<'info>,
 }
