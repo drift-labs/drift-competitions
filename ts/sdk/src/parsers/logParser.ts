2bdd6f0b6947cd9e4d34a069a93cbe95bc6ef15c
Chester Sim <chestersimdy@gmail.com>
2023-10-10 13:55:50 +0800

    refactor(sdk): handle empty logs in logparser

--- ts/sdk/src/parsers/logParser.ts
+++ ts/sdk/src/parsers/logParser.ts
@@ -10,6 +10,8 @@ export class LogParser {
 	public parseEventsFromLogs(event: Log): WrappedEvents {
 		const records: WrappedEvents = [];
 
+		if (!event.logs) return records;
+
 		// @ts-ignore
 		const eventGenerator = this.program._events._eventParser.parseLogs(
 			event.logs,
