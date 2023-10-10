f0a97ded5987adbbecd8ed59d3d529194793e56a
Chester Sim <chestersimdy@gmail.com>
2023-10-10 16:55:19 +0800

    refactor(sdk): add program errors for ui usage

--- ts/sdk/src/constants/index.ts
+++ ts/sdk/src/constants/index.ts
@@ -1,5 +1,8 @@
 import { PublicKey } from '@drift-labs/sdk';
 
+import DriftCompetitionsErrors from './driftCompetitionsErrors.json';
+export { DriftCompetitionsErrors };
+
 export const DRIFT_COMPETITION_PROGRAM_ID = new PublicKey(
 	'8yCtqd9UetSttHhbGJFRk3MpcQny3bNvEfv5YrADuHEp'
 );
---
+++ ts/sdk/src/scripts/update-drift-competitions-errors.ts
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+const driftCompetitionIdl = require('../idl/drift_competitions.json');
+
+let uiErrors = {
+	errorsList: {},
+	errorCodesMap: {}
+};
+
+const DRIFT_ERRORS_PATH = path.resolve(
+	__dirname,
+	'../constants/driftCompetitionsErrors.json'
+);
+
+try {
+	uiErrors = require(DRIFT_ERRORS_PATH);
+} catch (e) {
+	console.log("ui errors file doesn't exist yet");
+}
+
+// errorCodesMap should get reset every time, because the numbers could potentially change in the protocol's output
+// UI will use this to map the error code (number) to the name, so we can keep our manually added messages identified by the name but independent of the number
+uiErrors.errorCodesMap = {};
+
+driftCompetitionIdl.errors.forEach((err) => {
+	uiErrors.errorCodesMap[err.code] = err.name;
+	if (!uiErrors.errorsList[err.name]) {
+		uiErrors.errorsList[err.name] = err;
+	} else {
+		// Only update error code in errorsList
+		uiErrors.errorsList[err.name].code = err.code;
+	}
+});
+
+const copyErrorCodes = () => {
+	fs.writeFileSync(
+		DRIFT_ERRORS_PATH,
+		`${JSON.stringify(uiErrors, null, '	')}\n`
+	);
+};
+
+copyErrorCodes();
