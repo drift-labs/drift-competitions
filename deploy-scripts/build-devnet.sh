d8e92101147944d927d8da1fbf6511a794ecbf9c
Chris Heaney <chrisheaney30@gmail.com>
2023-12-22 11:49:04 -0600

    add deploy scripts

---
+++ deploy-scripts/build-devnet.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+anchor build
---
+++ deploy-scripts/deploy-devnet.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+anchor upgrade --program-id DraWMeQX9LfzQQSYoeBwHAgM5JcqFkgrX7GbTfjzVMVL --provider.cluster devnet --provider.wallet $SOLANA_PATH/$DEVNET_ADMIN target/deploy/drift_competitions.so
